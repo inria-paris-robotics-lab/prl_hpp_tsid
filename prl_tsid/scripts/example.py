ae6071c6af631726a7c4eadf0317d9bfcb23e0e9
EtienneAr <earlaud@pl346-pro.paris.inria.fr-1>
2021-11-08 16:31:45 +0100

    Joints limits

--- prl_tsid/scripts/example.py
+++ prl_tsid/scripts/example.py
@@ -1,5 +1,8 @@
-from prl_tsid.robots.ur5 import tsid, np, rospy, tsid_robot, formulation, robot, commander_left_arm, commander_right_arm
+import tsid
+import rospy
+import numpy as np
 import pinocchio as pin
+from prl_tsid.robots.ur5 import robot, tsid_robot, formulation, commander_left_arm, commander_right_arm
 
 qref = np.array([-1.55687287e+00, -1.56574731e+00, -1.56062175e+00, 2.40284589e-02, 1.61412316e+00, -7.95469352e-01, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 0.00000000e+00, 1.55664212e+00, -1.57508802e+00, 1.56221337e+00, 3.11631454e+00, -1.61955921e+00, 7.73079284e-01, 4.43863538e-04, 4.43863538e-04, 4.43863538e-04, 4.43863538e-04, 4.43863538e-04, 4.43863538e-04])
 vref = np.zeros(tsid_robot.nv)
@@ -9,18 +12,29 @@ my_K = 10
 postureTask = tsid.TaskJointPosture("task-posture", tsid_robot)
 postureTask.setKp(my_K* np.ones(tsid_robot.na))
 postureTask.setKd(2.0 * np.sqrt(my_K) * np.ones(tsid_robot.na))
-formulation.addMotionTask(postureTask, 1000, 1, 0.0)
+formulation.addMotionTask(postureTask, 1, 1, 0.0)
 
+tau_max = tsid_robot.model().effortLimit
+tau_min = - tau_max
+actuationBoundsTask = tsid.TaskActuationBounds("task-actuation-bounds", tsid_robot)
+actuationBoundsTask.setBounds(tau_min, tau_max)
+formulation.addActuationTask(actuationBoundsTask, 1, 0, 0.0)
+
+v_max = tsid_robot.model().velocityLimit
+v_min = - v_max
+jointBoundsTask = tsid.TaskJointBounds("task-joint-bounds", tsid_robot, 0.1)
+jointBoundsTask.setVelocityBounds(v_min, v_max)
+# jointBoundsTask.setMask(mask)
+formulation.addMotionTask(jointBoundsTask, 1, 0, 0.0)
 
 trajPosture = tsid.TrajectoryEuclidianConstant("traj_joint", qref)
-postureTask.setReference(trajPosture.computeNext())
 samplePosture = trajPosture.computeNext()
 
 solver = tsid.SolverHQuadProgFast("qp solver")
 solver.resize(formulation.nVar, formulation.nEq, formulation.nIn)
 
 
-i = 0
+i = 1
 dt = 0.1
 r = rospy.Rate(2. / dt)
 t = rospy.Time.now()
@@ -46,10 +60,13 @@ while True:
     HQPData = formulation.computeProblemData(t.to_sec(), np.array(q_meas), np.array(v_meas))
     sol = solver.solve(HQPData)
     if(sol.status!=0):
-        print("Time %.3f QP problem could not be solved! Error code:"%t, sol.status)
+        print(F"Time {t} QP problem could not be solved! Error code: {sol.status}")
         break
     
     # tau = formulation.getActuatorForces(sol)
+    # if i%25==0:
+    #     rospy.logwarn(tau)
+    
     dv_next = formulation.getAccelerations(sol)
 
     # numerical integration

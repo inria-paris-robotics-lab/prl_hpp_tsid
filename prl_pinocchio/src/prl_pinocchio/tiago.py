49972f320809236e18c39da32b0b32b6f3588cf8
earlaud <etienne.arlaud@inria.fr>
2022-02-25 14:17:44 +0100

    Propper upload.launch and q_meas

--- prl_pinocchio/src/prl_pinocchio/tiago.py
+++ prl_pinocchio/src/prl_pinocchio/tiago.py
@@ -1,6 +1,8 @@
 import rospy
-import xml.etree.ElementTree as ET
 
+import pinocchio as pin
+from sensor_msgs.msg import JointState
+from prl_pinocchio.tools.observer import Observer
 from prl_pinocchio.robot import Robot
 from prl_pinocchio.commander import Commander
 """
@@ -12,7 +14,10 @@ class Tiago_Robot(Robot):
     # right_gripper_name = "r_gripper"
 
     def __init__(self, robot_description_param_prefix, joint_state_topic):
-        Robot.__init__(self, robot_description_param_prefix, joint_state_topic)
+        Robot.__init__(self, robot_description_param_prefix)
+
+        self.joint_state_topic = joint_state_topic
+        self._joint_state_obs = Observer(joint_state_topic, JointState)
 
         # Init joints group
         joints = self.get_joint_names()
@@ -22,20 +27,44 @@ class Tiago_Robot(Robot):
         self.left_arm_joints  = list(filter(lambda joint: joint.lower().find("left")  != -1 and joint.lower().find("gripper") == -1, joints))
         self.right_arm_joints = list(filter(lambda joint: joint.lower().find("right") != -1 and joint.lower().find("gripper") == -1, joints))
 
-    # def get_gripper_link(self, gripper):
-    #     srdf = ET.fromstring(self.get_srdf_explicit())
+    def _get_raw_meas_qvtau(self):
+        """
+        Get the current position, velocity and effort of everyjoint of the robot.
 
-    #     gripper = srdf.find(".//gripper[@name='l_gripper']")
-    #     if gripper is None:
-    #         rospy.logerr(F"Could not find gripper {gripper} in robot srdf")
-    #         return None
+        Read it from the ros 'joint_state_topic' topic.
 
-    #     link = gripper.find("link")
-    #     if link is None:
-    #         rospy.logwarn(F"No link information found in srdf for gripper {gripper}")
-    #         return None
+        Optionnals parameters:
+        ----------------------
+            raw (bool): If not set to True, the configuration will be adjusted to fit in the joints bounds.
 
-    #     return link.attrib["name"]
+        Returns
+        -------
+            q (float[]): the configuration.
+            v (float[]): the velocities.
+            tau (float[]): the efforts.
+
+        Raises
+        ------
+            AssertionError: If the adjusted configuration deviates too much from the original one.
+        """
+        q = list(pin.neutral(self.pin_robot_wrapper.model))
+        v = [0] * self.pin_robot_wrapper.model.nv
+        tau = [0] * self.pin_robot_wrapper.model.nv
+
+        msg = self._joint_state_obs.get_last_msg()
+
+        for ros_idx, joint_name in enumerate(msg.name):
+            pin_index = self.pin_robot_wrapper.model.names.tolist().index(joint_name)
+            pin_nq = self.pin_robot_wrapper.model.joints[pin_index].nq
+            if pin_nq != 1:
+                continue
+            pin_idxq = self.pin_robot_wrapper.model.joints[pin_index].idx_q
+            pin_idxv = self.pin_robot_wrapper.model.joints[pin_index].idx_v
+            q[pin_idxq] = msg.position[ros_idx]
+            v[pin_idxv] = msg.velocity[ros_idx]
+            tau[pin_idxv] = msg.effort[ros_idx]
+
+        return q, v, tau
 
 def robot():
     rospy.logwarn("Create Tiago Robot...")

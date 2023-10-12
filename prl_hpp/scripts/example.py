8101d83572cf1636765c11a0908257985c8549e9
EtienneAr <etienne.arlaud@gmail.com>
2023-10-12 15:03:15 +0200

    Change planner example

--- prl_hpp/scripts/example.py
+++ prl_hpp/scripts/example.py
@@ -10,24 +10,25 @@ planner.lock_right_arm()
 planner.set_velocity_limit(0.25)
 planner.set_acceleration_limit(0.25)
 
+commander_left_arm.start_trajectory()
+
 # Get current pose
-q_curr = robot.get_meas_q()
-planner.v(q_curr)
+# q_curr = robot.get_meas_q()
+# planner.v(q_curr)
 
 pose_1 = [[-0.20, 0.00, 0.4], [pi/2, 0, pi/2]]
-pose_2 = [[-0.50, 0.30, 0.4], [pi/2, 0, pi]]
+pose_2 = [[-0.40, 0.20, 0.4], [pi/2, 0, pi]]
 
-paths = planner.make_pick_and_place(robot.left_gripper_name, pose_1, pose_2, approach_distance = 0.2)
-
-
-input("Solution found. Press enter to display path")
-planner.pp(paths[0].id)
-# input("Press enter to display path")
-planner.pp(paths[1].id)
-# input("Press enter to display path")
-planner.pp(paths[2].id)
-
+path = planner.make_gripper_approach(robot.left_gripper_name, pose_1, approach_distance = 0.2)
+# input("Solution found. Press enter to display path")
+# planner.pp(path.id)
 input("Solution found. Press enter to execute path")
-commander_left_arm.execute_path(paths[0])
-commander_left_arm.execute_path(paths[1])
-commander_left_arm.execute_path(paths[2])
+commander_left_arm.execute_path(path)
+
+# paths = planner.make_pick_and_place(robot.left_gripper_name, pose_1, pose_2, approach_distance = 0.2)
+
+# input("Solution found. Press enter to display paths")
+# planner.pp(paths[0].id)
+# planner.pp(paths[1].id)
+# planner.pp(paths[2].id)
+
